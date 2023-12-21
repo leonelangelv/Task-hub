5527878dcaa290efe6b28269aa7070f6bb29f700
Leonel Angel <leonelgo66@gmail.com>
2023-12-21 15:51:33 -0300

    feat: add icon more and image

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -4,3 +4,5 @@ export { checkmark_icon } from './icons/checkmark_icon';
 export { xmark_icon } from './icons/xmark_icon';
 export { trash_icon } from './icons/trash_icon';
 export { pen_icon } from './icons/pen_icon';
+export { more_icon } from './icons/more_icon';
+export {image_icon } from './icons/image_icon';
