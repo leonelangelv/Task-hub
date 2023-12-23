a45648016b4f118bb3529b4d9cd1fd85e1c2e8ee
Leonel Angel <leonelgo66@gmail.com>
2023-12-23 20:08:53 -0300

    add no tasks images

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -5,4 +5,6 @@ export { xmark_icon } from './icons/xmark_icon';
 export { trash_icon } from './icons/trash_icon';
 export { pen_icon } from './icons/pen_icon';
 export { more_icon } from './icons/more_icon';
-export {image_icon } from './icons/image_icon';
+export { image_icon } from './icons/image_icon';
+
+export { default as noTasks_image } from './images/no_tasks.png';
